8b43989c2314ce2f1738f50ccd1bc8bf9c333e32
Mark Munkacsi <munkacsimark@gmail.com>
2019-08-19 19:16:31 +0200

    Contact section added

--- src/layout/contact/Contact.tsx
+++ src/layout/contact/Contact.tsx
@@ -1,8 +1,51 @@
 import React from 'react';
+import { viewNames } from '../../view-router';
+import styles from './Contact.module.scss';
 
 const Contact: React.FunctionComponent = () => {
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // TODO handle submit
+    // TODO validation
+  }
+
   return (
-    <section>CONTACT</section>
+    <section
+      id={viewNames.CONTACT}
+      className={styles.section}>
+      <h2 className={styles.title}>
+        Contact
+      </h2>
+      <div className={styles.text}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam commodo viverra.
+      </div>
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit}>
+        <input
+          className={styles.input}
+          type="text"
+          name="name"
+          placeholder="What's your name?"/>
+        <input
+          className={styles.input}
+          type="email"
+          name="email"
+          placeholder="What's your email?"/>
+        <textarea
+          className={styles.textarea}
+          placeholder="How can I help you?"></textarea>
+        <button
+          className={styles.sendButton}
+          type="submit">
+          Send message
+        </button>
+      </form>
+      <span className={styles.copyText}>
+        Created with &#9829; by me - &copy; {(new Date()).getFullYear()}
+      </span>
+    </section>
   );
 }
 
