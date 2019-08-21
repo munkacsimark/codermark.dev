2882a25e501843fc1dbcf8d83488b05f6306279c
Mark Munkacsi <munkacsimark@gmail.com>
2019-08-22 01:37:53 +0200

    Form without async stuff for Formspree verification

--- src/layout/contact/Contact.tsx
+++ src/layout/contact/Contact.tsx
@@ -2,12 +2,32 @@ import React from 'react';
 import { viewNames } from '../../view-router';
 import styles from './Contact.module.scss';
 
+interface IFormData {
+  name: string,
+  email: string,
+  message: string,
+}
+
 const Contact: React.FunctionComponent = () => {
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // TODO handle submit
-    // TODO validation
+  const nameInput = React.createRef<HTMLInputElement>();
+  const emailInput = React.createRef<HTMLInputElement>();
+  const messageInput = React.createRef<HTMLTextAreaElement>();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    /*event.preventDefault();
+    const data: IFormData = {
+      name: nameInput.current ? nameInput.current.value : '',
+      email: emailInput.current ? emailInput.current.value : '',
+      message: messageInput.current ? messageInput.current.value : '',
+    }
+    const fetchOptions: RequestInit = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: JSON.stringify(data)
+    };
+    fetch('https://formspree.io/munkacsimark@gmail.com', fetchOptions)
+      .catch((error: Error) => console.error(error));*/
   }
 
   return (
@@ -18,22 +38,27 @@ const Contact: React.FunctionComponent = () => {
         Contact
       </h2>
       <div className={styles.text}>
-      Want to get in touch or talk about a project? Feel free to contact me here.
+        Want to get in touch or talk about a project? Feel free to contact me here.
       </div>
       <form
+        action="https://formspree.io/munkacsimark@gmail.com"
+        method="POST"
         className={styles.form}
         onSubmit={handleSubmit}>
         <input
+          ref={nameInput}
           className={styles.input}
           type="text"
           name="name"
           placeholder="What's your name?"/>
         <input
+          ref={emailInput}
           className={styles.input}
           type="email"
           name="email"
           placeholder="What's your email?"/>
         <textarea
+          ref={messageInput}
           className={styles.textarea}
           placeholder="How can I help you?"></textarea>
         <button
